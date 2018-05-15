9646c553d2074f087dd9409cfbcb80e896aaea77
Micah Shute <micah.shute@gmail.com>
2018-05-15 03:18:12 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,22 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+$content = Array.new(9, " ")
+
+def create_row(x, content)
+  pos = (x - 1) * 3
+  spaceContent(content[pos]) + '|' + spaceContent(content[pos + 1]) + '|' + spaceContent(content[pos + 2]) + "\n"
+end
+
+def spaceContent(content)
+  " #{content} "
+end
+
+def create_divider()
+  "-----------" + "\n"
+end
+
+def create_board(content)
+  create_row(0, content) + create_divider + create_row(1, content) + create_divider + create_row(2, content)
+end
+
+def display_board()
+  puts(create_board($content))
+end
